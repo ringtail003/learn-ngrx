42e55c591489aff8b2c493b89ee64d50f5ec2509
ringtail003 <ringtail003@gmail.com>
2020-01-12 22:49:38 +0900

    ページを開くたびにgetが走る

--- src/app/services/movies-repository.service.ts
+++ src/app/services/movies-repository.service.ts
@@ -14,6 +14,8 @@ export class MoviesRepositoryService {
   ) { }
 
   get(): Rx.Observable<Movie[]> {
+    console.info('get called.');
+
     // return this.httpClient.get<Movie[]>('/hoge/fuga').pipe(
     //   catchError((error: HttpErrorResponse) => {
     //     throw new Error(`${error.status} ${error.statusText}`);
